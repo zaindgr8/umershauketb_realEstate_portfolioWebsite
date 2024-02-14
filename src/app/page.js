0b5c4f591482c20c9dd0979755c18708c6869fcd
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-02-14 17:37:42 +0400

    14feb

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,11 @@
-import Header from "@/components/Header";
-import Carousel from "@/components/corousel";
-import FooterWhite from "@/components/footer-white";
-import Dropdown from "@/components/form-control-two";
-import Link from "next/link";
-import Firstsection from "./umerb/firstsection";
-import ProfileCard from "@/components/feature-properties";
 import Layout from "@/components/Layout";
-import SectionHeader from "@/components/section-header";
-import Regions from "@/components/Regions";
 import TopSection from "./umerb/TopSection";
 import SecondSection from "./umerb/thirdsection";
 import ThirdSection from "@/components/magnific-pop-up-gallery";
 import Testimonial from "./umerb/Testimonial";
 import Blogsection from "./umerb/blogsection";
 import Team from "./umerb/team";
-
+import TeamCorousal from "@/components/corouselTeam";
 
 export default function HomeTwo() {
   return (
@@ -29,6 +20,9 @@ export default function HomeTwo() {
           <Testimonial />
           <Blogsection />
           <Team />
+          <div className="p-3 mb-3">
+            <TeamCorousal />
+          </div>
         </>
       </main>
     </Layout>
---
+++ src/components/corouselTeam.js
@@ -0,0 +1,340 @@
+"use client";
+import dynamic from "next/dynamic";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Link from "next/link";
+
+var $ = require("jquery");
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = require("jquery");
+}
+
+// This is for Next.js. On Rect JS remove this line
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
+const Carousel = () => {
+  const options = {
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  };
+
+  return (
+    <OwlCarousel className="owl-theme blog-carousel" {...options}>
+      {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/az4.jpeg"
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              <Link href="https://www.facebook.com/azim.awan16" target="_blank">
+                <i className="fab fa-facebook-f" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.instagram.com/azamcreates/"
+                target="_blank"
+              >
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/muhammad-tariq-622211152/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Director
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">
+            Muhammad Azam Tariq
+          </h4>
+          <div>
+            Experienced real estate visionary leading with strategic insight.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */}
+      {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/dua.jpeg"
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              <Link
+                href="https://www.facebook.com/profile.php?id=100093387365067"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f" />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.instagram.com/douaa_hq/" target="_blank">
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/douaa%2Dharraq/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Off-Plan Consultant
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">Douaa Reina</h4>
+          <div>
+            Passionate real estate agent specializing in off-plan properties
+            within Dubai's dynamic market.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */}
+      {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/gulli.jpg"
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              <Link
+                href="https://www.facebook.com/gulraiz.naveed"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.instagram.com/therealtaholic/"
+                target="_blank"
+              >
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/gulraiz-naveed-043655230/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Sales Manager
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">Gulraiz Naveed</h4>
+          <div>
+            Driving results through strategic leadership and a passion for
+            exceeding.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */}
+      {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/mrgo.png"
+              className="h-full w-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              <Link
+                href="https://www.facebook.com/profile.php?id=100006375667786"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.instagram.com/pearllrealestate/"
+                target="_blank"
+              >
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/margault-almeras-60785515a/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Off-Plan Consultant
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">Margo Pearl</h4>
+          <div>
+            navigating clients through the exciting world of pre-construction
+            real estate.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */}
+      {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/waled.jpeg"
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              <Link
+                href="https://www.facebook.com/waleedkhanyousafzai"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.instagram.com/reltaholic/"
+                target="_blank"
+              >
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/company/crescorealestatellc/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Sales Executive
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">
+            Waleed Khan Yousafzai
+          </h4>
+          <div>
+            dedicated to delivering exceptional client experiences and achieving
+            sales success.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */} {/* Start Grid Post */}
+      <div className="team-member">
+        <div className="member-header overflow-hidden position-relative rounded-4">
+          <div className="w-full h-[45vh] overflow-hidden">
+            <img
+              src="assets/img/avatar/vrutika.jpeg"
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <ul className="align-items-center d-flex flex-column fs-21 gap-2 justify-content-center list-unstyled mb-0 member-social position-absolute">
+            <li>
+              {/* <Link
+                href="https://www.facebook.com/waleedkhanyousafzai"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f" />
+              </Link> */}
+            </li>
+            <li>
+              <Link href="@vruti_cresco" target="_blank">
+                <i className="fab fa-instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/vrutika-dhanak-69843a2b4/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in" />
+              </Link>
+            </li>
+          </ul>
+          <div className="bg-dark bottom-1 end-1 position-absolute px-3 py-1 rounded-5 team-description-wrap text-white z-1">
+            Administration
+          </div>
+        </div>
+        {/* Start Avatar Info */}
+        <div className="member-info text-center mt-3">
+          <h4 className="mb-2 member-name text-truncate">Vrutika Dhanak</h4>
+          <div>
+            From managing schedules to handling inquiries, she streamline
+            administrative tasks.
+          </div>
+        </div>
+        {/* /.End Avatar Info */}
+      </div>
+      {/* /.End Grid Post */}
+    </OwlCarousel>
+  );
+};
+export default Carousel;
